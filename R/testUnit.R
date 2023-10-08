12876574f92e13578cb6fea0af6f3498dd3c2e2d
AnastasiaPia <118008882+AnastasiaPia@users.noreply.github.com>
2023-10-08 21:28:01 +0200

    tests-change

---
+++ R/testUnit.R
@@ -0,0 +1,31 @@
+source('Lab5.R')
+
+#Function check
+test_that("bikeStationStatu functionality", {
+  # Create a sample city_info object
+  sample_city_info <- list(
+    network = list(
+      stations = data.frame(
+        name = c("Station A", "Station B"),
+        free_bikes = c(10, 5),
+        extra = list(address = c("Address A", "Address B")),
+        empty_slots = c(5, 10)
+      )
+    )
+  )
+
+  results <- bikeStationStatu(sample_city_info)
+
+  # Test that the function returns a list
+  expect_is(results, "list")
+
+  # Busy check
+  expect_equal(results$busiest$name, "Station A")
+  expect_equal(results$busiest$empty_slots, 5)
+  expect_equal(results$busiest$free_bikes, 10)
+
+  #least busy check
+  expect_equal(results$least_busy$name, "Station B")
+  expect_equal(results$least_busy$empty_slots, 10)
+
+})
